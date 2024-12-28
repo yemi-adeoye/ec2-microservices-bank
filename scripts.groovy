46c691245d916dbd19fb749184710344d32a9ade
yemi-adeoye <adeyemi_adeoye@hotmail.com>
2024-12-28 16:20:44 -0600

    Adds groovy script

---
+++ scripts.groovy
@@ -0,0 +1,17 @@
+def buildUsersMicroservice(){
+    echo "Building users microservice"
+}
+
+def connectToEc2(){
+    echo "Building users microservice"
+}
+
+def copyJarToEc2(){
+    echo "Copying Files to EC2"
+}
+
+def runUsersMicroSErvice(){
+    echo "Running users microservice"
+}
+
+return this
