c97e2734cd410cbed9cd8b68f659d6603770ee3a
yemi-adeoye <adeyemi_adeoye@hotmail.com>
2024-12-28 23:49:38 -0600

    changes ssh command flag

--- scripts.groovy
+++ scripts.groovy
@@ -8,7 +8,7 @@ def connectToEc2() {
     echo 'Connecting to EC2'
     sh 'cd /var/jenkins_home/workspace'
     sh 'ls'
-    sh 'ssh -T -i kp-my-server.pem ec2-user@ec2-52-90-228-109.compute-1.amazonaws.com'
+    sh 'ssh -i kp-my-server.pem ec2-user@ec2-52-90-228-109.compute-1.amazonaws.com'
 }
 
 def copyJarToEc2() {
