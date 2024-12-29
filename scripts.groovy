32404b594e1f94bbe4c0a43d9b6a7d14c3a038a2
yemi-adeoye <adeyemi_adeoye@hotmail.com>
2024-12-29 02:48:35 -0600

    updates groovy script

--- scripts.groovy
+++ scripts.groovy
@@ -8,7 +8,7 @@ def connectToEc2() {
     sshagent(['2']) {
         echo 'Connecting to EC2'
         sh 'ssh -o StrictHostKeyChecking=no ec2-user@ec2-3-88-248-51.compute-1.amazonaws.com'
-        sh 'scp -i ${keyfile} ./target/banks-ms-0.0.1-SNAPSHOT.jar ec2-user@ec2-3-88-248-51.compute-1.amazonaws.com:/bank'
+        sh 'scp -i ${keyfile} ./target/banks-ms-0.0.1-SNAPSHOT.jar ec2-user@ec2-3-88-248-51.compute-1.amazonaws.com:/'
         sh 'java -jar banks-ms-0.0.1-SNAPSHOT.jar'
     }  
 }
