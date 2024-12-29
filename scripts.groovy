4b6aef280d61b6978194075d8ec7b94393240703
yemi-adeoye <adeyemi_adeoye@hotmail.com>
2024-12-29 03:31:52 -0600

    updates groovy script scp command

--- scripts.groovy
+++ scripts.groovy
@@ -8,7 +8,11 @@ def connectToEc2() {
     sshagent(['2']) {
         echo 'Connecting to EC2'
         sh 'scp -i ${keyfile} ./target/banks-ms-0.0.1-SNAPSHOT.jar ec2-user@ec2-3-88-248-51.compute-1.amazonaws.com:~'
-        sh 'ssh -o StrictHostKeyChecking=no ec2-user@ec2-3-88-248-51.compute-1.amazonaws.com && cd ~ && java -jar banks-ms-0.0.1-SNAPSHOT.jar'
+        // sh 'ssh -o StrictHostKeyChecking=no ec2-user@ec2-3-88-248-51.compute-1.amazonaws.com'
+        sh '''
+            ec2-user@ec2-3-88-248-51.compute-1.amazonaws.com <<'ENDSSH'
+            cd ~ && java -jar banks-ms-0.0.1-SNAPSHOT.jar
+        ENDSSH'''
     }  
 }
 
