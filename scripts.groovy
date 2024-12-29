7ed5074424340ea937cf3384629684674859a1a6
yemi-adeoye <adeyemi_adeoye@hotmail.com>
2024-12-29 00:15:25 -0600

    changes ssh command flag

--- scripts.groovy
+++ scripts.groovy
@@ -1,14 +1,16 @@
 /* groovylint-disable CompileStatic, FactoryMethodName, MethodReturnTypeRequired, NoDef */
 def buildUsersMicroservice() {
     echo 'Building users microservice'
-    // sh 'mvn clean package'
+// sh 'mvn clean package'
 }
 
 def connectToEc2() {
-    echo 'Connecting to EC2'
-    sh 'cd ../'
-    sh 'ls'
-    sh 'ssh -T -i kp-my-server.pem ec2-user@ec2-52-90-228-109.compute-1.amazonaws.com'
+    sshagent(['2']) {
+        echo 'Connecting to EC2'
+        sh 'cd ../'
+        sh 'ls'
+        sh 'ssh -T -i kp-my-server.pem ec2-user@ec2-52-90-228-109.compute-1.amazonaws.com'
+    }  
 }
 
 def copyJarToEc2() {
