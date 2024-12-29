777fb512a9bed573d37c18ce1cc61fd7a91d0c19
yemi-adeoye <adeyemi_adeoye@hotmail.com>
2024-12-29 01:46:29 -0600

    updates groovy script

--- scripts.groovy
+++ scripts.groovy
@@ -7,9 +7,9 @@ def buildUsersMicroservice() {
 def connectToEc2() {
     sshagent(['2']) {
         echo 'Connecting to EC2'
-        sh 'cd ../'
-        sh 'ls'
-        sh 'ssh -T -i kp-my-server.pem ec2-user@ec2-52-90-228-109.compute-1.amazonaws.com'
+        sh 'ssh -o ec2-user@ec2-18-206-39-47.compute-1.amazonaws.com'
+        sh 'scp /target/banks-ms-0.0.1-SNAPSHOT.jar ec2-user@ec2-18-206-39-47.compute-1.amazonaws.com:/bank'
+        sh 'java -jar banks-ms-0.0.1-SNAPSHOT.jar'
     }  
 }
 
