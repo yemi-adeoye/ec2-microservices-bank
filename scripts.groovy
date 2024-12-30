b681bf1e952f651e2d066c6055284450c722dd27
yemi-adeoye <adeyemi_adeoye@hotmail.com>
2024-12-30 17:52:26 -0600

    adds runservice function

--- scripts.groovy
+++ scripts.groovy
@@ -7,7 +7,7 @@ def buildUsersMicroservice() {
 
 def copyJarToEC2() {
     echo 'Connecting to EC2'
-    sh '''scp -i ${keyfile} -o StrictHostKeyChecking=no ./target/banks-ms-0.0.1-SNAPSHOT.jar ${USER}@${HOST}:~/banks-ms/'''
+    sh '''scp -i ${keyfile} -o StrictHostKeyChecking=no ./target/banks-ms-0.0.1-SNAPSHOT.jar ${USER}@${HOST}:~/banks-ms/bank-ms.jar'''
 }
 
 def runUsersMicroService() {
