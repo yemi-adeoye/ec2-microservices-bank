8d7a736fbacb5d144fca6a5ce823ebe20b7b73fb
Yemi Adeoye <adeyemi_adeoye@hotmail.com>
2024-12-30 17:05:10 +0000

    Edit scripts.groovy

--- scripts.groovy
+++ scripts.groovy
@@ -1,12 +1,12 @@
 /* groovylint-disable CompileStatic, FactoryMethodName, MethodReturnTypeRequired, NoDef */
 def buildUsersMicroservice() {
     echo 'Building users microservice'
-    sh 'mvn clean package -DskipTests'
+    // sh 'mvn clean package -DskipTests'
 }
 
 def connectToEc2() {
         echo 'Connecting to EC2'
-        sh 'scp -i ${keyfile} -o StrictHostKeyChecking=no ./target/banks-ms-0.0.1-SNAPSHOT.jar ec2-user@ec2-52-90-34-240.compute-1.amazonaws.com:~'
+        sh 'scp -i ${keyfile} -o StrictHostKeyChecking=no ./target/banks-ms-0.0.1-SNAPSHOT.jar ec2-user@ec2-52-90-34-240.compute-1.amazonaws.com:~/bank-ms'
 }
 
 def copyJarToEc2() {
