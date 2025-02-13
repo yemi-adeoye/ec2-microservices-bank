d3821e2a3743838351784371ded6d0daae4bda60
yemi-adeoye <adeyemi_adeoye@hotmail.com>
2025-02-13 05:06:45 -0600

    adds groovy local and jenkins local

---
+++ scripts.local.groovy
@@ -0,0 +1,18 @@
+/* groovylint-disable CompileStatic, FactoryMethodName, MethodReturnTypeRequired, NoDef */
+
+def buildUsersMicroservice() {
+    echo 'Building users microservice'
+    sh 'mvn clean package -DskipTests'
+}
+
+def copyJarToEC2() {
+    echo 'Copying Jar to EC2 Mock'
+    sh '''scp -p ${LOCAL_EC2_PASSWORD} -o StrictHostKeyChecking=no ./target/banks-ms-0.0.1-SNAPSHOT.jar ${LOCAL_USER}@${BANK_MS_LOCALHOST}:~/${BANK_MS_DIR}/${BANK_MS_FILE}'''
+}
+
+def runUsersMicroService() {
+    echo 'Running users microservice'
+    sh '''nohup ssh -i ${keyfile} -o StrictHostKeyChecking=no ${USER}@${HOST} "cd ${BANK_MS_DIR} &&  java -jar ${BANK_MS_FILE} " &'''
+}
+
+return this
