bba8ea6cbb61a24d90f8f3ae0bf04577af70ff85
yemi-adeoye <adeyemi_adeoye@hotmail.com>
2025-02-13 05:22:04 -0600

    fixes typo in local groovy script

--- scripts.local.groovy
+++ scripts.local.groovy
@@ -7,7 +7,7 @@ def buildUsersMicroservice() {
 
 def copyJarToEC2() {
     echo 'Copying Jar to EC2 Mock'
-    sh '''echo${LOCAL_EC2_PASSWORD}'''
+    sh '''echo ${LOCAL_EC2_PASSWORD}'''
     // sh '''scp -p ${LOCAL_EC2_PASSWORD} -o StrictHostKeyChecking=no ./target/banks-ms-0.0.1-SNAPSHOT.jar ${LOCAL_USER}@${BANK_MS_LOCALHOST}:~/${BANK_MS_DIR}/${BANK_MS_FILE}'''
 }
 
