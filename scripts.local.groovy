a19fa120fba180e35665a48244a01dc48a9c8987
yemi-adeoye <adeyemi_adeoye@hotmail.com>
2025-02-14 17:24:57 -0600

    updates scripts file

--- scripts.local.groovy
+++ scripts.local.groovy
@@ -7,12 +7,12 @@ def buildUsersMicroservice() {
 
 def copyJarToEC2() {
     echo 'Copying Jar to EC2 Mock'
-    sh '''sshpass -p ${LOCAL_EC2_PASSWORD} scp -o StrictHostKeyChecking=no ./target/banks-ms-0.0.1-SNAPSHOT.jar ${USER}@${BANK_MS_LOCALHOST}:/home/ec2-user/${BANK_MS_DIR}/${BANK_MS_FILE}'''
+    sh '''sshpass -p ${LOCAL_EC2_PASSWORD} scp -o StrictHostKeyChecking=no ./target/banks-ms-0.0.1-SNAPSHOT.jar ${USER}@${BANK_MS_LOCALHOST}:/${BANK_MS_DIR}/${BANK_MS_FILE}'''
 }
 
 def runUsersMicroService() {
     echo 'Running users microservice'
-    sh '''sshpass -p ${LOCAL_EC2_PASSWORD} ssh ${USER}@${BANK_MS_LOCALHOST} " java -jar /home/ec2-user/${BANK_MS_DIR}/${BANK_MS_FILE} " &'''
+    sh '''nohup sshpass -p ${LOCAL_EC2_PASSWORD} ssh ${USER}@${BANK_MS_LOCALHOST} " java -jar /${BANK_MS_DIR}/${BANK_MS_FILE} " &'''
 }
 
 return this
